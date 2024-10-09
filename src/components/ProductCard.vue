ef12bc2addf2a9a6251eaaf50bf8224f9eeffc89
iooi-alice <kimhee21c@hanmail.net>
2024-10-09 23:03:45 +0900

    feat: product card 컴포넌트 구현, 타이틀 클릭 시 모달 상태 변경 및 상품 id 전달 method 구현

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-for="product in oneroomList" :key="product.id">
+    <h2>{{ product.id }}</h2>
+    <img class="room-img" :src="product.image" alt="원룸 내부 이미지" />
+    <h4 @click="send(product)">{{ product.title }}</h4>
+    <p>{{ product.content }}</p>
+    <span>{{ product.price }} 원</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    oneroomList: Array,
+  },
+  methods: {
+    send(product) {
+      this.$emit('openModal', product.id);
+    },
+  },
+};
+</script>
+
+<style>
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+</style>
