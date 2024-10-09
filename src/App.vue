059966a939be0b6f44acdc793ea68b9575a61048
iooi-alice <kimhee21c@hanmail.net>
2024-10-09 23:06:20 +0900

    feat: DiscountPopup, ProductModal, ProductCard 컴포넌트 App.vue에 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="nav">
+    <nav>
+      <a href="/" v-for="navItem in navList" :key="navItem">{{ navItem }}</a>
+    </nav>
+  </header>
+  <DiscountPopup />
+
+  <ProductCard
+    @openModal="
+      isOpen = true;
+      productId = $event;
+    "
+    :oneroomList="oneroomList"
+    :openModal="openModal"
+  />
+  <ProductModal @closeModal="isOpen = false" :oneroomList="oneroomList" :productId="productId" :isOpen="isOpen" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/oneroom';
+import DiscountPopup from './components/DiscountPopup';
+import ProductModal from './components/ProductModal';
+import ProductCard from './components/ProductCard';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      productId: 0,
+      oneroomList: data,
+      isOpen: false,
+      countList: [0, 0, 0],
+      products: [
+        { city: '역삼동 원룸', cost: '50만원', image: require('@/assets/room0.jpg') },
+        { city: '천호동 원룸', cost: '60만원', image: require('@/assets/room1.jpg') },
+        { city: '마포구 원룸', cost: '70만원', image: require('@/assets/room2.jpg') },
+      ],
+      navList: ['Home', 'Products', 'About', 'Shop'],
+    };
+  },
+  methods: {
+    increase(i) {
+      this.countList[i]++;
+    },
+  },
   components: {
-    HelloWorld
-  }
-}
+    DiscountPopup,
+    ProductModal,
+    ProductCard,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +59,30 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.nav {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.nav a {
+  color: #fff;
+  padding: 10px;
+  text-decoration: none;
+}
+
+.discount {
+  background: #eee;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 5px;
 }
 </style>
